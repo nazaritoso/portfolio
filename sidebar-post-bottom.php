3ec72189b84ccf7dbc9b0c71c27c26243d5549b3
Nazar <nazaritoso@gmail.com>
2017-09-18 17:42:47 +0300

    4

--- sidebar-post-bottom.php
+++
@@ -1,5 +0,0 @@
-<?php
-if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('post-bottom') ):
-//here default sidebar content
-endif;
-?>
--- sidebar-post-top.php
+++
@@ -1,5 +0,0 @@
-<?php
-if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('post-top') ):
-//here default sidebar content
-endif;
-?>
